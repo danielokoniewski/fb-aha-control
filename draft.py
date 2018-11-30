ebefb988c160a73a0416f551bb91d90fb75cffd6
Daniel Okoniewski <daniel.okoniewski@osdj.de>
2018-11-30 14:00:02 +0100

    working session create

--- draft.py
+++ draft.py
@@ -1,16 +1,26 @@
+import time
+import hashlib
+import sys
 import http.client
+import urllib.parse
 import xml.dom.minidom as mdom
 
 base_url='fritz.box'
 
-def getText(nodelist):
-    rc = []
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            rc.append(node.data)
-    return ''.join(rc)
+def readLoginResponse(responseText):
+    sid = None
+    challenge = None
+    blocktime = 0
+    dom = mdom.parseString(responseText)
+    # add assert
+    sessioninfo = dom.getElementsByTagName('SessionInfo')[0]
+    sid = sessioninfo.getElementsByTagName('SID')[0].firstChild.nodeValue
+    challenge = sessioninfo.getElementsByTagName('Challenge')[0].firstChild.nodeValue
+    blocktime = sessioninfo.getElementsByTagName('BlockTime')[0].firstChild.nodeValue
+    return {'sid':sid, 'challenge':challenge, 'blocktime': blocktime}
 
-def getSessionID(base_url):
+
+def getChallenge(base_url):
     sid = None
     challenge = None
     #ask for new sid
@@ -19,23 +29,41 @@ def getSessionID(base_url):
     response = conn.getresponse()
     if response.status == 200:
         resp_content = response.read()
-        #read result
-        dom = mdom.parseString(resp_content)
-        # add assert
-        sessioninfo = dom.getElementsByTagName('SessionInfo')[0]
-        sid = sessioninfo.getElementsByTagName('SID')[0].firstChild.nodeValue
-        challenge = sessioninfo.getElementsByTagName('Challenge')[0].firstChild.nodeValue
+        res = readLoginResponse(resp_content)
+        challenge = res['challenge']
+        # wait bock time
+        blocktime = int(res['blocktime'])
+        time.sleep(blocktime/1000.0)
+    return challenge
 
-    return {sid, challenge}
+def getResponse(challenge, password):
+    bstring = '-'.join([challenge, password]).encode('utf-16le')
+    bstringHash = hashlib.md5(bstring).hexdigest()
+    response = '-'.join([challenge,bstringHash])
+    return response
 
+def getSessionID(base_url, username, password):
+    sid = None
+    challenge = getChallenge(base_url)
+    response = getResponse(challenge, password)
+    conn = http.client.HTTPConnection(base_url)
+    params = urllib.parse.urlencode({'username':username, 'response':response})
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn.request('POST', '/login_sid.lua', params, headers )
+    response = conn.getresponse()
+    if response.status == 200:
+        response_text = response.read()
+        res= readLoginResponse(response_text)
+        sid=res['sid']
+    return sid
 
-def doThings():
+def doThings(sid):
+    #"http://fritz.box/home/home.lua?sid=<sid>"
     print ("hallo")
 
 
 if __name__ == '__main__':
     # set sesion is
     # use sesionid for calls
-    sid, challenge = getSessionID(base_url)
+    sid = getSessionID(base_url, username=sys.argv[1] , password=sys.argv[2])
     print(sid)
-    print(challenge)
